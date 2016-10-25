ce63309c1a317eed7052a14d8aed08dcda831686
Jasmine Dahilig <jasmine.dahilig@gmail.com>
2016-10-25 11:25:44 -0700

    add linked list and partial mergesort

---
+++ code/LinkedList.java
@@ -0,0 +1,56 @@
+class LinkedList {
+
+    class Node {
+        Node next;
+        String value;
+
+        public Node(Node next, String value) {
+            this.next = next;
+            this.value = value;
+        }
+    }
+
+    // instance variables
+    Node first;
+    int n;
+
+    // construct an empty linked list
+    public LinkedList() {
+        first = null;
+        n = 0;
+    }
+
+    public void add(String s) {
+        Node newFirst = new Node(first, s);
+        first = newFirst;
+        n++;
+    }
+
+    public String removeFirst() {
+        String temp = first.value;
+        first = first.next;
+        n--;
+        return temp;
+    }
+
+    // linear search for s and remove it if it exists
+    public void remove(String s) {
+      Node current = first;
+      Node prev = null;
+      if (first.value.equals(s)) {
+        first = first.next;
+        n--;
+        return;
+      }
+      while (current != null) {
+        if (current.value.equals(s)) {
+          prev.next = current.next;
+          n--;
+          return;
+        }
+        prev = current;
+        current = current.next;
+      }
+    }
+
+}
---
+++ code/MergeSort.java
@@ -0,0 +1,53 @@
+public class MergeSort {
+
+  public static void main(String[] args) {
+
+  }
+
+  public static void mergesort(int[] array) {
+    int[] temp = new int[array.length];
+    mergesort(array, temp, 0, array.length - 1);
+
+  }
+
+  public static void mergesort(int[] array, int[] temp, int left, int right) {
+    if (left < right) {
+      int mid = (left + right) / 2;
+      mergesort(array, temp, left, mid);
+      mergesort(array, temp, mid + 1, right);
+      merge(array, temp, left, mid, right);
+    }
+  }
+
+  public static void merge(int[] array, int[] temp, int left, int mid, int right) {
+    // copy sorted subarrays into temp array
+    for (int i = left; i <= right; i++) {
+      temp[i] = array[i];
+    }
+
+    // interleave the subarrays:
+    // start with each array's lowest element
+    
+    int arrayIndex = left;
+    int leftIndex = left;
+    int rightIndex = right;
+
+    // interleave until you run out of elements
+    while (leftIndex <= mid && rightIndex <= right) {
+      // compare which element is lower
+      // and copy the lower element into the original array
+      if (temp[leftIndex] <= temp[rightIndex]) {
+        array[arrayIndex] = temp[leftIndex];
+        leftIndex++;
+      } else {
+        array[arrayIndex] = temp[rightIndex];
+        rightIndex++;
+      }
+      arrayIndex++;
+
+    }
+
+    // copy the remaining left elements back to the original array
+
+  }
+}
