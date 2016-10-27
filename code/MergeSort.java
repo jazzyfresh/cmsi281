f1a4679ca06d5c7b4ddaadae2ff44dbdddb48fab
Jasmine Dahilig <jasmine.dahilig@gmail.com>
2016-10-26 22:51:37 -0700

    add rest of merge function

--- code/MergeSort.java
+++ code/MergeSort.java
@@ -1,53 +1,58 @@
 public class MergeSort {
 
-  public static void main(String[] args) {
-
-  }
-
-  public static void mergesort(int[] array) {
-    int[] temp = new int[array.length];
-    mergesort(array, temp, 0, array.length - 1);
-
-  }
-
-  public static void mergesort(int[] array, int[] temp, int left, int right) {
-    if (left < right) {
-      int mid = (left + right) / 2;
-      mergesort(array, temp, left, mid);
-      mergesort(array, temp, mid + 1, right);
-      merge(array, temp, left, mid, right);
-    }
-  }
-
-  public static void merge(int[] array, int[] temp, int left, int mid, int right) {
-    // copy sorted subarrays into temp array
-    for (int i = left; i <= right; i++) {
-      temp[i] = array[i];
-    }
-
-    // interleave the subarrays:
-    // start with each array's lowest element
-    
-    int arrayIndex = left;
-    int leftIndex = left;
-    int rightIndex = right;
-
-    // interleave until you run out of elements
-    while (leftIndex <= mid && rightIndex <= right) {
-      // compare which element is lower
-      // and copy the lower element into the original array
-      if (temp[leftIndex] <= temp[rightIndex]) {
-        array[arrayIndex] = temp[leftIndex];
-        leftIndex++;
-      } else {
-        array[arrayIndex] = temp[rightIndex];
-        rightIndex++;
-      }
-      arrayIndex++;
+    public static void main(String[] args) {
 
     }
 
-    // copy the remaining left elements back to the original array
+    public static void mergesort(int[] array) {
+        int[] temp = new int[array.length];
+        mergesort(array, temp, 0, array.length - 1);
+    }
 
-  }
+    public static void mergesort(int[] array, int[] temp, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergesort(array, temp, left, mid);
+            mergesort(array, temp, mid + 1, right);
+            merge(array, temp, left, mid, right);
+        }
+    }
+
+    public static void merge(int[] array, int[] temp, int left, int mid, int right) {
+        // First: Copy sorted subarrays into temp array:
+        for (int i = left; i <= right; i++) {
+            temp[i] = array[i];
+        }
+
+        // Second: Interleave the subarrays:
+        //
+        // start with each array's lowest element
+        int arrayIndex = left;
+        int leftIndex = left;
+        int rightIndex = right;
+
+        // interleave until you run out of elements
+        while (leftIndex <= mid && rightIndex <= right) {
+            // compare which element is lower
+            // and copy the lower element into the original array
+            if (temp[leftIndex] <= temp[rightIndex]) {
+                array[arrayIndex] = temp[leftIndex];
+                leftIndex++;
+            } else {
+                array[arrayIndex] = temp[rightIndex];
+                rightIndex++;
+            }
+            arrayIndex++;
+        }
+
+        // Third: copy the remaining left elements back to the original array
+        while (leftIndex <= mid) {
+            array[arrayIndex] = temp[leftIndex];
+            leftIndex++;
+            arrayIndex++;
+        }
+
+    }
+
+
 }
